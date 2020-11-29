4f1736e8b951ca3668575aaa7bfeb70810866a9b
Johan Bendz <Johan_Bendz@hotmail.com>
2020-11-30 00:24:10 +0100

    v.0.1.16

---
+++ drivers/doorwindowsensor2/device.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Homey = require('homey');
+const { ZigBeeDevice } = require('homey-zigbeedriver');
+const { debug, CLUSTER } = require('zigbee-clusters');
+
+class doorwindowsensor2 extends ZigBeeDevice {
+		
+	async onNodeInit({zclNode}) {
+
+		this.enableDebug();
+		debug(true);
+		this.printNode();
+
+		// alarm_contact
+      zclNode.endpoints[1].clusters[CLUSTER.IAS_ZONE.NAME].onZoneStatusChangeNotification = payload => {
+        this.onIASZoneStatusChangeNotification(payload);
+      }
+
+  }
+  
+  onIASZoneStatusChangeNotification({zoneStatus, extendedStatus, zoneId, delay,}) {
+    this.log('IASZoneStatusChangeNotification received:', zoneStatus, extendedStatus, zoneId, delay);
+    this.setCapabilityValue('alarm_contact', zoneStatus.alarm1);
+    this.setCapabilityValue('alarm_battery', zoneStatus.battery);
+  }
+
+	onDeleted(){
+		this.log("Door/Window Sensor removed")
+	}
+
+}
+
+module.exports = doorwindowsensor2;
