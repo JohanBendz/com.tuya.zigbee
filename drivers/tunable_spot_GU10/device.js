52c23884d5095a0cda7ddbf36c396bac451d542b
Johan Bendz <Johan_Bendz@hotmail.com>
2021-02-07 21:03:42 +0100

    v.0.1.41

--- drivers/tunable_spot_GU10/device.js
+++ drivers/tunable_spot_GU10/device.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { ZigBeeLightDevice } = require('homey-zigbeedriver');
+const TuyaZigBeeLightDevice = require('../../lib/TuyaZigBeeLightDevice');
 
-class tunable_spot_GU10 extends ZigBeeLightDevice {}
+class tunable_spot_GU10 extends TuyaZigBeeLightDevice {}
 
 module.exports = tunable_spot_GU10;
