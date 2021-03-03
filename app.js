96ede11eef79a9a17550397ae8d44eecd611fe0e
Johan Bendz <Johan_Bendz@hotmail.com>
2021-03-03 23:11:03 +0100

    v.0.1.45 Fix for Tunable Lights

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Homey = require('homey');
-const { debug } = require('zigbee-clusters');
-debug(true);
+//const { debug } = require('zigbee-clusters');
+//debug(true);
 
 class tuyazigbee extends Homey.App {
 	
@@ -10,7 +10,7 @@ class tuyazigbee extends Homey.App {
 		this.log('Tuya Zigbee app is running...');
 
 		this.homey.flow.getActionCard('start_effect').registerRunListener(async (args, state) => {
-			this.log("ACTION TRIGGERED");
+			this.log("Christmas Lights Action Triggered");
 			await args.christmas_lights_device.StartEffect(args);
 			return true
 		})
