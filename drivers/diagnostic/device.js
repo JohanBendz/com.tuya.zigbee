15d3302e57e102998c584b3d97c7f88050da7325
Johan Bendz <Johan_Bendz@hotmail.com>
2021-03-15 00:01:20 +0100

    v.0.1.50

--- drivers/diagnostic/device.js
+++ drivers/diagnostic/device.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const Homey = require('homey');
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { debug, CLUSTER } = require('zigbee-clusters');
+const { CLUSTER } = require('zigbee-clusters');
 
 class diagnostic extends ZigBeeDevice {
 
     async onNodeInit({zclNode}) {
 
-        this.printNode();
-		debug(true);
-    
-    const node = await this.homey.zigbee.getNode(this);
-		node.handleFrame = (endpointId, clusterId, frame, meta) => {
-        this.log("endpointId:", endpointId,", clusterId:", clusterId,", frame:", frame, ", meta:", meta);
-        this.log("Frame JSON data:", frame.toJSON());
-    };
+      this.printNode();
+   
+      const node = await this.homey.zigbee.getNode(this);
+      node.handleFrame = (endpointId, clusterId, frame, meta) => {
+          this.log("endpointId:", endpointId,", clusterId:", clusterId,", frame:", frame, ", meta:", meta);
+          this.log("Frame JSON data:", frame.toJSON());
+      };
 
     }
 
