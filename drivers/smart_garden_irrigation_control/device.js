ca8da85c4ca5b20618a90d8a2e66f7b8b982166a
rdl987 <raffdelauri@gmail.com>
2022-06-16 17:05:45 +0200

    some improvments

--- drivers/smart_garden_irrigation_control/device.js
+++ drivers/smart_garden_irrigation_control/device.js
@@ -2,8 +2,10 @@
 
 const Homey = require('homey');
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { debug, CLUSTER } = require('zigbee-clusters');
-//const DeviceApi = require('device-api');
+const { Cluster, debug, CLUSTER } = require('zigbee-clusters');
+const TuyaSpecificCluster = require('../../lib/TuyaSpecificCluster');
+
+Cluster.addCluster(TuyaSpecificCluster);
 
 const DEFAULT_ONOFF_DURATION = 1000
 
@@ -17,17 +19,20 @@ class IrrigationController extends ZigBeeDevice {
     this.registerCapabilityListener("onoff", async (value, options) =>{
       this.log("value "+value);
       this.log("options "+options.duration);
-      if (options.duration != undefined){
-        await zclNode.endpoints[1].clusters['onOff'].toggle();
+      if (value && options.duration != undefined ){
+        await zclNode.endpoints[1].clusters['onOff'].setOn();
         await new Promise(resolve => setTimeout(resolve, options.duration));
-        await zclNode.endpoints[1].clusters['onOff'].toggle();
-      }else{
-        await zclNode.endpoints[1].clusters['onOff'].toggle();
+        await zclNode.endpoints[1].clusters['onOff'].setOff();
+      }else if(value && options.duration === undefined){
+        await zclNode.endpoints[1].clusters['onOff'].setOn();
+      }else if(!value && options.duration === undefined){
+        await zclNode.endpoints[1].clusters['onOff'].setOff();
       }
-      
     });
+
     // measure_battery // alarm_battery
-		zclNode.endpoints[1].clusters[CLUSTER.POWER_CONFIGURATION.NAME].on('attr.batteryPercentageRemaining', this.onBatteryPercentageRemainingAttributeReport.bind(this));
+		zclNode.endpoints[1].clusters[CLUSTER.POWER_CONFIGURATION.NAME]
+		.on('attr.batteryPercentageRemaining', this.onBatteryPercentageRemainingAttributeReport.bind(this));
 
   }
 
@@ -37,8 +42,6 @@ class IrrigationController extends ZigBeeDevice {
 
   onBatteryPercentageRemainingAttributeReport(batteryPercentageRemaining) {
 		const batteryThreshold = this.getSetting('batteryThreshold') || 20;
-		this.log("measure_battery | powerConfiguration - batteryPercentageRemaining (%): ", batteryPercentageRemaining/2);
-		this.setCapabilityValue('measure_battery', batteryPercentageRemaining/2);
 		this.setCapabilityValue('alarm_battery', (batteryPercentageRemaining/2 < batteryThreshold) ? true : false)
 	}
 
