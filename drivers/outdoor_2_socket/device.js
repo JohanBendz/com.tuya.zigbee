a94cdd46c3159afee9ac98bedba11823674e4bf4
Julian Meisel <1023650+neoscaler@users.noreply.github.com>
2024-01-01 23:00:33 +0100

    add images, fix metering

--- drivers/outdoor_2_socket/device.js
+++ drivers/outdoor_2_socket/device.js
@@ -9,30 +9,10 @@ Cluster.addCluster(TuyaOnOffCluster);
 class outdoor2socket extends ZigBeeDevice {
 
   async onNodeInit({zclNode}) {
-
-    await zclNode.endpoints[1].clusters.basic.readAttributes('manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 'attributeReportingStatus')
-    .catch(err => {
-        this.error('Error when reading device attributes ', err);
-    });
+    const { subDeviceId } = this.getData();
 
     this.printNode();
-    
-    const { subDeviceId } = this.getData();
-    this.log('Device data: ', subDeviceId);
-
-    this.meteringOffset = this.getSetting('metering_offset');
-    this.measureOffset = this.getSetting('measure_offset') * 100;
-    this.minReportPower = this.getSetting('minReportPower') * 1000;
-    this.minReportCurrent = this.getSetting('minReportCurrent') * 1000;
-    this.minReportVoltage = this.getSetting('minReportVoltage') * 1000;
-
-    if (!this.hasCapability('measure_current')) {
-      await this.addCapability('measure_current').catch(this.error);
-    }
-
-    if (!this.hasCapability('measure_voltage')) {
-      await this.addCapability('measure_voltage').catch(this.error);
-    }
+    this.log("Device data: ", subDeviceId);
 
     // onOff
     this.registerCapability('onoff', CLUSTER.ON_OFF, {
@@ -40,72 +20,76 @@ class outdoor2socket extends ZigBeeDevice {
       getOpts: {
         getOnStart: true,
         pollInterval: 60000
-      }
+	    }
     });
 
-    // Endpoint 1
-    let options1 = { endpoint: 1 };
-    this.registerCapabilities(zclNode, options1);
+    if (!this.isSubDevice()) {
+      this.meteringOffset = this.getSetting('metering_offset');
+      this.measureOffset = this.getSetting('measure_offset') * 100;
+      this.minReportPower= this.getSetting('minReportPower') * 1000;
+      this.minReportCurrent = this.getSetting('minReportCurrent') * 1000;
+      this.minReportVoltage = this.getSetting('minReportVoltage') * 1000;
 
-    // Endpoint 2
-    let options2 = { endpoint: 2 };
-    this.registerCapabilities(zclNode, options2);
-
-  }
-
-  registerCapabilities(zclNode, options) {
-
-
-    // meter_power
-    this.registerCapability('meter_power', CLUSTER.METERING, options, {
-      reportParser: value => (value * this.meteringOffset)/100.0,
-      getParser: value => (value * this.meteringOffset)/100.0,
-      getOpts: {
-        getOnStart: true,
-        pollInterval: 300000
+      if (!this.hasCapability('measure_current')) {
+        await this.addCapability('measure_current').catch(this.error);
       }
-    });
 
-    // measure_power
-    this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, options, {
-      reportParser: value => {
-        return (value * this.measureOffset)/100;
-      },
-      getOpts: {
-        getOnStart: true,
-        pollInterval: this.minReportPower
+      if (!this.hasCapability('measure_voltage')) {
+        await this.addCapability('measure_voltage').catch(this.error);
       }
-    });
 
-    this.registerCapability('measure_current', CLUSTER.ELECTRICAL_MEASUREMENT, options, {
-      reportParser: value => {
-        return value/1000;
-      },
-      getOpts: {
-        getOnStart: true,
-        pollInterval: this.minReportCurrent
-      }
-    });
+      // meter_power
+      this.registerCapability('meter_power', CLUSTER.METERING, {
+        reportParser: value => (value * this.meteringOffset)/100.0,
+        getParser: value => (value * this.meteringOffset)/100.0,
+        getOpts: {
+          getOnStart: true,
+          pollInterval: 300000
+        }
+      });
 
-    this.registerCapability('measure_voltage', CLUSTER.ELECTRICAL_MEASUREMENT, options, {
-      reportParser: value => {
-        return value;
-      },
-      getOpts: {
-        getOnStart: true,
-        pollInterval: this.minReportVoltage
-      }
-    });
+      // measure_power
+      this.registerCapability('measure_power', CLUSTER.ELECTRICAL_MEASUREMENT, {
+        reportParser: value => {
+          return (value * this.measureOffset)/100;
+          },
+        getOpts: {
+          getOnStart: true,
+          pollInterval: this.minReportPower
+        }
+      });
+
+      this.registerCapability('measure_current', CLUSTER.ELECTRICAL_MEASUREMENT, {
+        reportParser: value => {
+          return value/100;
+          },
+        getOpts: {
+          getOnStart: true,
+          pollInterval: this.minReportCurrent
+        }
+      });
+
+      this.registerCapability('measure_voltage', CLUSTER.ELECTRICAL_MEASUREMENT, {
+        reportParser: value => {
+          return value;
+          },
+        getOpts: {
+          getOnStart: true,
+          pollInterval: this.minReportVoltage
+        }
+      });
+
+      await zclNode.endpoints[1].clusters.basic.readAttributes('manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 'attributeReportingStatus')
+        .catch(err => {
+          this.error('Error when reading device attributes ', err);
+        });
+    }
+    
   }
 
   onDeleted() {
-    this.log("Outdoor 2 Socket removed")
+    this.log("Double Outdoor Smart 2 Socket removed");
   }
-
-  async onSettings({oldSettings, newSettings, changedKeys}) {
-
-  }
-  
 }
 
 module.exports = outdoor2socket;
