cda761f745a63dceb5fc831eedd74bcc43f4b71f
Stefan Schweiger <stefan@5gpay.com>
2024-02-08 23:04:10 +0100

    Add min and max brightness to dimmer_1_gang

--- drivers/dimmer_1_gang/device.js
+++ drivers/dimmer_1_gang/device.js
@@ -1,29 +1,60 @@
-'use strict';
+"use strict";
 
-const Homey = require('homey');
-const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { debug, CLUSTER } = require('zigbee-clusters');
+const Homey = require("homey");
+const { ZigBeeDevice } = require("homey-zigbeedriver");
+const {
+    debug,
+    CLUSTER,
+} = require("zigbee-clusters");
+
+const { mapValueRange, calculateLevelControlTransitionTime } = require("../../lib/util");
 
 class dimmer_1_gang extends ZigBeeDevice {
-
-    async onNodeInit({zclNode}) {
-
+    async onNodeInit({ zclNode }) {
         this.printNode();
 
-        this.registerCapability('onoff', CLUSTER.ON_OFF);
-        this.registerCapability('dim', CLUSTER.LEVEL_CONTROL);
+        this.registerCapability("onoff", CLUSTER.ON_OFF);
 
-        await zclNode.endpoints[1].clusters.basic.readAttributes('manufacturerName', 'zclVersion', 'appVersion', 'modelId', 'powerSource', 'attributeReportingStatus')
-        .catch(err => {
-            this.error('Error when reading device attributes ', err);
+        this.registerCapability("dim", CLUSTER.LEVEL_CONTROL, {
+            setParser: async (value, opts) => {
+                let level = 0;
+
+                if (value >= 1) {
+                    level = 255;
+                } else if (value > 0) {
+                    level = Math.ceil(
+                        mapValueRange(
+                            0,
+                            1,
+                            this.getSetting("minBrightness"),
+                            this.getSetting("maxBrightness"),
+                            value
+                        )
+                    );
+                }
+
+                await this.setCapabilityValue('onoff', value > 0);
+
+                return {
+                    level,
+                    transitionTime: calculateLevelControlTransitionTime(opts)
+                };
+            },
+            reportParser: (value) => {
+                return mapValueRange(
+                    this.getSetting("minBrightness"),
+                    this.getSetting("maxBrightness"),
+                    0,
+                    1,
+                    value
+                );
+            },
         });
-
     }
 
-    onDeleted(){
-		this.log("1 Gang Dimmer removed")
-	}
-
+    onDeleted() {
+        this.log("1 Gang Dimmer removed");
+    }
 }
 
 module.exports = dimmer_1_gang;
