377993d4a7507c07f7929d4e1bb55adfd9813cc9
Johan Bendz <Johan_Bendz@hotmail.com>
2024-08-31 16:23:28 +0200

    v.0.2.27

---
+++ drivers/rgb_ceiling_led_light/device.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const TuyaZigBeeLightDevice = require('../../lib/TuyaZigBeeLightDevice');
+
+class rgb_ceiling_led_light extends TuyaZigBeeLightDevice {}
+
+module.exports = rgb_ceiling_led_light;
