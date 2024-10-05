f6a57909c2f7b2f8993c69fc4a18565e0b1076a6
Johan Bendz <Johan_Bendz@hotmail.com>
2024-10-05 21:59:57 +0200

    v.0.2.65

--- drivers/thermostatic_radiator_valve/driver.js
+++ drivers/thermostatic_radiator_valve/driver.js
@@ -6,16 +6,7 @@ const Homey = require("homey");
 class ThermostaticRadiatorValveDriver extends ZigBeeDriver {
 
     async onInit() {
-
-        this.homey.flow.getActionCard("window_open_status_set")
-            .registerRunListener(async ({ device, message }, state) => {
-                await device.setWindowOpen(state.value);
-            });
-
-        this.homey.flow.getConditionCard("window_open_status_get")
-            .registerRunListener(async ({ device, message }, state) => {
-                return device.getWindowOpen();
-            });
+        this.log('Thermostatic Radiator Valve Driver initialized');
     }
 }
 
