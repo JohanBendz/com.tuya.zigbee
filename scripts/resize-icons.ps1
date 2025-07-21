8d867a71b796f7a8f068d8b98f4df5df818e147f
senetmarne@gmail.com <ton@email.com>
2025-07-21 22:05:44 +0200

    feat: add icon-resize script & ignore .env

---
+++ scripts/resize-icons.ps1
@@ -0,0 +1,9 @@
+﻿param([int]$icon=64,[int]$large=500)
+Get-ChildItem drivers -Recurse -Filter *.png | ForEach-Object {
+  $d = $_.DirectoryName
+  $iconPath  = Join-Path $d "assets/icon.png"
+  $largePath = Join-Path $d "assets/large.png"
+  $null = New-Item -ItemType Directory -Path (Split-Path $iconPath) -Force
+  magick $_.FullName -resize "${icon}x${icon}!" $iconPath
+  magick $_.FullName -resize "${large}x${large}!" $largePath
+}
