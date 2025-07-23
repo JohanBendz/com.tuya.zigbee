542a58406c2ed77676f61c932ec5d01d7feab9a9
senetmarne@gmail.com <ton@email.com>
2025-07-23 16:46:49 +0200

    refactor: fusion forcée des fichiers TS130F et TS0207 dans curtain_module et rain_sensor. Nettoyage des doublons, organisation conforme, suppression des dossiers sources. Préparation à l'enrichissement automatique via BDD Zigbee2MQTT et Home Assistant.

--- drivers/TS0207/device.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const { ZigBeeDevice } = require('homey-zigbeedriver');
-class TS0207 extends ZigBeeDevice {}
-module.exports = TS0207;
--- drivers/TS130F/device.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const { ZigBeeDevice } = require('homey-zigbeedriver');
-class TS130F extends ZigBeeDevice {}
-module.exports = TS130F;
